b10f3cf5ed0629838380af711b97dc5ebab3f389
abodell <andybodell9@gmail.com>
2025-01-20 11:53:14 -0500

    testing

--- backend/scripts/get-nfl-team-data.py
+++ backend/scripts/get-nfl-team-data.py
@@ -56,6 +56,7 @@ def insert_to_supabase(team_data: dict):
     except Exception as e:
         print(f"Error during bulk upsert: {e}")
 
+print('test')
 
 if __name__ == "__main__":
     client = httpx.Client(base_url = 'https://sports.yahoo.com/site/api/resource')
