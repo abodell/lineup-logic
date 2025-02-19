a3006dbfb38ada6f9955a87d59d4319dc5e15bfc
abodell <andybodell9@gmail.com>
2025-02-19 08:15:05 -0500

    Returned winning bets for spread, total, and ML for each game

--- backend/app/routers/betting.py
+++ backend/app/routers/betting.py
@@ -8,11 +8,10 @@ router = APIRouter()
 async def get_recent_betting_data(supabase: AsyncClient = Depends(get_supabase)):
     try:
         response = await supabase.table('latest_betting_lines').select('away_ml, home_ml, away_spread, home_spread, away_line, home_line, total, over_line, under_line, ' 
-                    'game:games!game_id(home_team_id, away_team_id, start_time, home_team:teams!Games_home_team_id_season_fkey(team_abbr), away_team:teams!Games_away_team_id_season_fkey(team_abbr))') \
+                    'game:games!game_id(home_team_id, away_team_id, start_time, home_team:teams!Games_home_team_id_season_fkey(team_abbr), away_team:teams!Games_away_team_id_season_fkey(team_abbr)), '
+                    'winning_ml_bet, winning_spread_bet, winning_total_bet') \
                     .limit(10) \
                     .execute()
-        
-        print(response.data)
 
         return response.data
 
